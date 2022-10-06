8493e292cf5d0c85e923531da4c983192a1557fe
alex <areygim2202@g.educaand.es>
2022-10-06 14:34:27 +0200

    Commit ubuntu 22.04 LTS

--- app/src/main/java/com/example/sumador/MainActivity.java
+++ app/src/main/java/com/example/sumador/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         et1=findViewById(R.id.editTextNumber);
         et2=findViewById(R.id.editTextNumber2);
         tv1=findViewById(R.id.textView);
+        et1.setText("0");
+        et2.setText("0");
     }
 
     public void sumar(View view) {
@@ -32,7 +34,49 @@ public class MainActivity extends AppCompatActivity {
         // Hacemos la suma
         int suma=n1+n2;
 
-        // Establecemos el resultado en el TextView
-        tv1.setText(""+suma);
+            // Establecemos el resultado en el TextView
+            tv1.setText("" + suma);
     }
+
+    public void restar(View view) {
+
+        int n1=Integer.parseInt(et1.getText().toString());
+        int n2=Integer.parseInt(et2.getText().toString());
+
+        // Hacemos la resta
+        int resta=n1-n2;
+
+            tv1.setText("" + resta);
+
+    }
+
+    public void multiplicar(View view) {
+
+        int n1=Integer.parseInt(et1.getText().toString());
+        int n2=Integer.parseInt(et2.getText().toString());
+
+        //Hacemos la multiplicación
+        int multiplicar=n1*n2;
+
+            tv1.setText("" + multiplicar);
+
+    }
+
+    public void dividir(View view) {
+
+        double dividir;
+
+        int n1 = Integer.parseInt(et1.getText().toString());
+        int n2 = Integer.parseInt(et2.getText().toString());
+
+        // Hacemos la división
+        if (n1 == 0 && n2 == 0) {
+            dividir = 0;
+        } else {
+            dividir = n1 / n2;
+        }
+        tv1.setText("" + dividir);
+
+    }
+
 }
